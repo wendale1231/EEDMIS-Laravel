a27c2c70d1bdf330af5734cd5507ecb071604d27
Remarc Balisi <remarc.balisi@fligno.com>
2019-09-02 12:20:25 +0800

    create stall detail controller and request

---
+++ app/Http/Controllers/Web/PublicMarket/StallDetailController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Web\PublicMarket;
+
+use App\Http\Requests\StallDetailStoreRequest;
+use App\StallDetail;
+use App\Http\Controllers\Controller;
+
+class StallDetailController extends Controller
+{
+    public function index()
+    {
+        return StallDetail::paginate();
+    }
+
+    public function show(StallDetail $stall_detail)
+    {
+        return $stall_detail;
+    }
+
+    public function store(StallDetailStoreRequest $request)
+    {
+        $new_stall_detail = StallDetail::create($request->all());
+        return $new_stall_detail;
+    }
+
+    public function update(StallDetailStoreRequest $request, StallDetail $stall_detail)
+    {
+        $stall_detail->update($request->all());
+        return $stall_detail;
+    }
+}
--- app/StallDetail.php
+++ app/StallDetail.php
@@ -9,7 +9,7 @@ class StallDetail extends Model
 
 	const UPDATED_AT = null;
     protected $fillable = [
-        'stall_type', 'availability',
+        'stall_type', 'availability', 'stall_code',
     ];
 
     protected $table = 'stall_details';
